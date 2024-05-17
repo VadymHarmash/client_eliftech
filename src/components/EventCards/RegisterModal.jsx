f7dc23cf3b1df515c380bc457ee85e92a5db20a0
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-05-17 10:28:01 +0300

    fixed

--- src/components/EventCards/RegisterModal.jsx
+++ src/components/EventCards/RegisterModal.jsx
@@ -38,7 +38,7 @@ export default function RegisterModal({ styles, onClose, eventId }) {
     const handleSubmit = async () => {
         if (!validateForm()) return;
         
-        const response = await fetch(`${process.env.SERVER}/${eventId}/participants`, {
+        const response = await fetch(`https://eliftech-server-f72u.onrender.com/events/${eventId}/participants`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
